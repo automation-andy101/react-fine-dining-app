c964ca33fe747efc3c045f5191a847d08d322447
Andy Short <automation.andy101@gmail.com>
2023-04-10 17:18:10 +0100

    Added footer section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { images } from '../../constants';
 
+
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem'}}>
     <p className='p__cormorant'>{title}</p>
